0ab42933d80c12c81c7430a3abcb92a604953519
Sime <bertoluchi2003@gmail.com>
2024-01-13 18:17:22 +0100

    protective model

--- src/main/java/com/example/wizardpedia/Models/Protective.java
+++ src/main/java/com/example/wizardpedia/Models/Protective.java
@@ -1,21 +1,23 @@
 package com.example.wizardpedia.Models;
 
+import lombok.Getter;
+
+@Getter
 public enum Protective {
 
-    ARMOR("Armor"),
-    SHIELD("Shield"),
-    CLOAK("Cloak"),
-    AMULET("Amulet"),
-    RING("Ring");
+    ARMOR("Armor", 100, 80),
+    SHIELD("Shield", 50, 60),
+    CLOAK("Cloak", 30, 40),
+    AMULET("Amulet", 20, 30),
+    RING("Ring", 10, 5);
 
     private String displayName;
+    private int price;
+    private int powerLevel;
 
-    Protective(String displayName) {
+    Protective(String displayName, int price, int powerLevel) {
         this.displayName = displayName;
+        this.price = price;
+        this.powerLevel = powerLevel;
     }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
 }
