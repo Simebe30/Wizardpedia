5f0576e08c257e4156cb63ea21caf74168833c2b
Sime <bertoluchi2003@gmail.com>
2024-01-13 18:18:03 +0100

    new changes magical item

--- src/main/java/com/example/wizardpedia/Models/MagicalItem.java
+++ src/main/java/com/example/wizardpedia/Models/MagicalItem.java
@@ -1,9 +1,15 @@
 package com.example.wizardpedia.Models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "magicalItems")
+@Getter
+@Setter
+@NoArgsConstructor
 public class MagicalItem {
 
 
@@ -14,54 +20,27 @@ public class MagicalItem {
     private String name;
 
     private int powerLevel;
-    private Protective protectiveItem;
 
+    private Protective protectiveItem;
 
     private String offensiveItem;
 
     @ManyToOne
     private Wizard wizard;
 
-    public MagicalItem() {
-    }
 
     public MagicalItem(String name, int powerLevel) {
         this.name = name;
         this.powerLevel = powerLevel;
     }
-
     public MagicalItem(String name, int powerLevel, Wizard wizard) {
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
-    }
-
-    public int getPowerLevel() {
-        return powerLevel;
-    }
-
-    public void setPowerLevel(int powerLevel) {
         this.powerLevel = powerLevel;
-    }
-
-    public Wizard getWizard() {
-        return wizard;
-    }
-
-    public void setWizard(Wizard wizard) {
         this.wizard = wizard;
     }
-
-    public void setId(Long id) {
-        this.id = id;
+    public MagicalItem(String name, Protective protective, Wizard wizard) {
+        this.name = name;
+        this.protectiveItem = protective;
     }
 
-    public Long getId() {
-        return id;
-    }
 }
