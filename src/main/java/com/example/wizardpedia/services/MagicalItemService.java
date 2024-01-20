f6529a401f17d64b179283e452b2a90ed782a23e
Sime <bertoluchi2003@gmail.com>
2024-01-20 20:24:15 +0100

    services

--- src/main/java/com/example/wizardpedia/services/MagicalItemService.java
+++ src/main/java/com/example/wizardpedia/services/MagicalItemService.java
@@ -1,7 +1,6 @@
 package com.example.wizardpedia.services;
 
-import com.example.wizardpedia.Models.MagicalItem;
-import com.example.wizardpedia.Models.Wizard;
+import com.example.wizardpedia.Models.*;
 import com.example.wizardpedia.repositories.MagicalItemRepository;
 import com.example.wizardpedia.repositories.WizardRepository;
 import org.springframework.stereotype.Service;
@@ -10,78 +9,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Service
-public class MagicalItemService {
+public interface MagicalItemService {
 
-    private final MagicalItemRepository magicalItemRepository;
-    private final WizardRepository wizardRepository;
+//    ProtectiveItem shopItem(Protective protective, Wizard wizard);
 
-    public MagicalItemService(MagicalItemRepository magicalItemRepository, WizardRepository wizardRepository) {
-        this.magicalItemRepository = magicalItemRepository;
-        this.wizardRepository = wizardRepository;
-    }
+    ProtectiveItem shopItem(Protective protective, Long wizardId);
 
+    MagicalItem shopItem();
 
-    public MagicalItem shopItem(MagicalItem magicalItem){
-
-
-        Wizard wizard  = wizardRepository.findWizardById(magicalItem.getWizard().getId()).get();
-        int wizardMoney = wizard.getCoins();
-//        int itemPrice = protective.getPrice();
-//        int currentWizardMoney = wizardMoney - itemPrice;
-
-//        wizard.setCoins(currentWizardMoney);
-
-        magicalItemRepository.save(magicalItem);
-        return magicalItem;
-    }
-
-    public MagicalItem addItem(MagicalItem magicalItem){
-        return magicalItemRepository.save(magicalItem);
-    }
-
-    public List<MagicalItem> getItemsByName(String itemName){
-        List<MagicalItem> magicalItem =  magicalItemRepository.findMagicalItemByNameContainingIgnoreCase(itemName);
-        if(magicalItem.isEmpty()){
-            return new ArrayList<>();
-        }
-        return magicalItem;
-    }
-
-    public boolean delete(Long id){
-        Optional<MagicalItem> maybeItem= magicalItemRepository.findById(id);
-        if(maybeItem.isEmpty()){
-            return false;
-        }else{
-            magicalItemRepository.deleteById(id);
-            return true;
-        }
-    }
-
-    public boolean updateMagicalItem(Long id, String name, int powerLevel){
-        Optional<MagicalItem> maybeItem= Optional.ofNullable(magicalItemRepository.findMagicalItemById(id));
-        if(maybeItem.isEmpty()){
-            return false;
-        }
-
-        MagicalItem magicalItem = maybeItem.get();
-        magicalItem.setName(name);
-        magicalItem.setPowerLevel(powerLevel);
-        magicalItemRepository.save(magicalItem);
-        return true;
-    }
-
-    public Optional<MagicalItem> getItemById(Long itemId) {
-        return magicalItemRepository.findById(itemId);
-    }
-
-    public List<MagicalItem> getItemsByWizId(Long wizardId){
-        return magicalItemRepository.findMagicalItemsByWizardId(wizardId);
-    }
-
-    public int getTotalScore(){
-        return magicalItemRepository.findAll().stream()
-                .mapToInt(MagicalItem::getPowerLevel)
-                .sum();
-    }
+    MagicalItem addItem(MagicalItem magicalItem);
+    List<MagicalItem> getItemsByName(String itemName);
+    boolean delete(Long id);
+    boolean updateMagicalItem(Long id, String name, int powerLevel);
+    List<MagicalItem> getItemsByWizId(Long wizardId);
+    int getTotalScore();
 }
+
---
+++ src/main/java/com/example/wizardpedia/services/ProtectiveService.java
@@ -0,0 +1,111 @@
+package com.example.wizardpedia.services;
+
+import com.example.wizardpedia.Models.MagicalItem;
+import com.example.wizardpedia.Models.Protective;
+import com.example.wizardpedia.Models.ProtectiveItem;
+import com.example.wizardpedia.Models.Wizard;
+import com.example.wizardpedia.repositories.MagicalItemRepository;
+import com.example.wizardpedia.repositories.ProtectiveRepository;
+import com.example.wizardpedia.repositories.WizardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProtectiveService implements MagicalItemService {
+
+    private final MagicalItemRepository magicalItemRepository;
+    private final WizardRepository wizardRepository;
+    private final ProtectiveRepository protectiveRepository;
+
+    @Autowired
+    public ProtectiveService(MagicalItemRepository magicalItemRepository, WizardRepository wizardRepository, ProtectiveRepository protectiveRepository) {
+        super();
+        this.magicalItemRepository = magicalItemRepository;
+        this.wizardRepository = wizardRepository;
+        this.protectiveRepository = protectiveRepository;
+    }
+
+    @Override
+    public ProtectiveItem shopItem(Protective protective, Long wizardId) {
+
+        Wizard wizard = wizardRepository.findWizardById(wizardId).get();
+        int wizardMoney = wizard.getCoins();
+        int itemPrice = protective.getPrice();
+        int currentWizardMoney = wizardMoney - itemPrice;
+        String itemName = protective.name().toLowerCase() + " the " + protective.getDisplayName().toLowerCase();
+
+        wizard.setCoins(currentWizardMoney);
+
+        ProtectiveItem protectiveItem = new ProtectiveItem(wizard, itemName, protective.getPowerLevel(), protective);
+        protectiveRepository.save(protectiveItem);
+        return protectiveItem;
+    }
+
+    @Override
+    public MagicalItem shopItem() {
+        return null;
+    }
+
+    @Override
+    public MagicalItem addItem(MagicalItem magicalItem) {
+        if (magicalItem instanceof ProtectiveItem) {
+            ProtectiveItem protectiveItem = (ProtectiveItem) magicalItem;
+            return protectiveRepository.save(protectiveItem);
+        } else {
+            // Handle the case where magicalItem is not a ProtectiveItem
+            return null; // or throw an exception, depending on your design
+        }
+    }
+
+    @Override
+    public List<MagicalItem> getItemsByName(String itemName){
+        List<MagicalItem> magicalItem =  magicalItemRepository.findMagicalItemByNameContainingIgnoreCase(itemName);
+        if(magicalItem.isEmpty()){
+            return new ArrayList<>();
+        }
+        return magicalItem;
+    }
+
+    @Override
+    public boolean delete(Long id){
+        Optional<MagicalItem> maybeItem= magicalItemRepository.findById(id);
+        if(maybeItem.isEmpty()){
+            return false;
+        }else{
+            magicalItemRepository.deleteById(id);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean updateMagicalItem(Long id, String name, int powerLevel){
+        Optional<MagicalItem> maybeItem= Optional.ofNullable(magicalItemRepository.findMagicalItemById(id));
+        if(maybeItem.isEmpty()){
+            return false;
+        }
+
+        MagicalItem magicalItem = maybeItem.get();
+        magicalItem.setName(name);
+        magicalItem.setPowerLevel(powerLevel);
+        magicalItemRepository.save(magicalItem);
+        return true;
+    }
+
+    @Override
+    public List<MagicalItem> getItemsByWizId(Long wizardId){
+        return magicalItemRepository.findMagicalItemsByWizardId(wizardId);
+    }
+
+    @Override
+    public int getTotalScore() {
+        return magicalItemRepository.findAll().stream()
+                .mapToInt(MagicalItem::getPowerLevel)
+                .sum();
+    }
+
+
+}
